5592dfe69f17ca129aadf64a1b716d653166c623
pontiiis01-glitch <pontiiis01@gmail.com>
2026-02-13 23:06:52 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import { saveAs } from "https://cdn.skypack.dev/file-saver";
 
 // === LISTAS E DADOS ===
 const UNIDADES_CBMMA_FIXAS = [
-    "1 BBM", "2 BBM", "1 CIEBM", "10 BBM", "13 BBM", "16 CIBM", "BBS", "BBA", "BMUS", "CGCS", "DEP", "DAT", "DP", "DF", "DPM", "DAL", "CPP", "CPO", 
+    "1 BBM", "2 BBM", "1 CIEBM", "10 BBM", "13 BBM", "16 CIBM", "BBS", "BBA", "BMUS","BBEM", "CGCS", "DEP", "DAT", "DP", "DF", "DPM", "DAL", "CPP", "CPO", 
     "1 Seção", "2 Seção", "3 Seção", "4 Seção", "CAPS", "CRF", "CEPDECMA", "ASPIRANTES", "CADETES", "DER", 
     "CMCB I", "CMCB II - SJR", "CMCB XII - PAÇO", "CMCB XIII - GUANABARA", "CMCB XXVI - PIO XII", 
     "ABMJM", "GAB CMT GERAL", "GAB CMT ADJUNTO"
